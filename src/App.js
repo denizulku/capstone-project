629c76394a1e5b380988937699c62affbc679cdf
denizulku <deniz.uelkue@gmail.com>
2020-08-31 15:37:07 +0200

    Edit App

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Form from './components/Form'
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Habit Tracker</h1>
-      </header>
+      <header></header>
       <Form />
     </div>
   )
