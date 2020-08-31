15632646a45ff80879aac5aa94373a7c0c20ea03
denizulku <deniz.uelkue@gmail.com>
2020-08-31 12:56:53 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import Form from "./components/Form";
-import styled from "styled-components";
+import React, { useState } from 'react'
+import styled from 'styled-components'
 
 function App() {
   return (
@@ -8,9 +7,8 @@ function App() {
       <header>
         <h1>Habit Tracker</h1>
       </header>
-      <Form />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
