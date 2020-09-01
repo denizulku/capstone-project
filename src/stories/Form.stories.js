35bd27a6f5020efffe02413d93b8bcbaa1ddeadb
denizulku <deniz.uelkue@gmail.com>
2020-09-01 12:56:49 +0200

    Add storybook form component

---
+++ src/stories/Form.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Input from '../../src/components/Form/Form'
+
+export default {
+  title: 'HabitInput',
+  component: Input,
+}
+export const Form = () => {
+  return <Form type="text" placeholder="Insert a habit" />
+}
