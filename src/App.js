272a078f853a673dc479cb0e9f796b7b01def601
denizulku <deniz.uelkue@gmail.com>
2020-09-07 17:56:16 +0200

    Add util.js

--- src/App.js
+++ src/App.js
@@ -8,38 +8,9 @@ function App() {
   return (
     <div className="App">
       <Form habits={habits} onSubmit={addHabit} />
-      <List
-        displayToday={displayToday}
-        habits={habits}
-        onItemClick={toggleCompleted}
-      />
+      <List habits={habits} onItemClick={toggleCompleted} />
     </div>
   )
-
-  function displayToday() {
-    const today = new Date()
-    const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ]
-    const date =
-      today.getDate() +
-      ' ' +
-      months[today.getMonth()] +
-      ' ' +
-      today.getFullYear()
-    return date
-  }
 
   function addHabit(name) {
     setHabits([...habits, { name, completed: false, id: uuid() }])
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import styled from 'styled-components/macro'
 import Habit from '../Habit/Habit'
+import { displayToday } from '../../util'
 
-export default function List({ habits, onItemClick, displayToday }) {
+export default function List({ habits, onItemClick }) {
   return (
     <>
       <CurrentDate>{displayToday()}</CurrentDate>
