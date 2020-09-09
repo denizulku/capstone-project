ddb7f679a7e2d76bca646edf53e929bd295557b9
denizulku <deniz.uelkue@gmail.com>
2020-09-09 12:01:01 +0200

    Complete App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,34 +1,33 @@
 import React from 'react'
-import { BrowserRouter, Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { render } from '@testing-library/react'
-import { createMemoryHistory } from 'history'
 import App from './App'
 
-describe('App.test.js', () => {
+describe('App.js', () => {
   it('renders Dashboard when the path exactly equals "/"', () => {
-    const { getByTestId } = render(
+    const { queryByText } = render(
       <BrowserRouter initialEntries={['/']}>
         <App />
       </BrowserRouter>
     )
-    expect(getByTestId('Welcome!'))
+    expect(queryByText('/'))
   })
 
   it('renders CreatePage when the path exactly equals "/create"', () => {
-    const { getByTestId } = render(
+    const { queryByText } = render(
       <BrowserRouter initialEntries={['/create']}>
         <App />
       </BrowserRouter>
     )
-    expect(getByTestId('Insert a habit'))
+    expect(queryByText('create'))
   })
 
   it('renders OverviewPage when the path exactly equals "/overview"', () => {
-    const { getByTestId } = render(
+    const { queryByText } = render(
       <BrowserRouter initialEntries={['/overview']}>
         <App />
       </BrowserRouter>
     )
-    expect(getByTestId('Calendar'))
+    expect(queryByText('overview'))
   })
 })
--- src/components/Navigation/Navigation.test.js
+++ src/components/Navigation/Navigation.test.js
@@ -1,43 +1,5 @@
-import React from 'react'
-import { BrowserRouter, Router } from 'react-router-dom'
-import { render } from '@testing-library/react'
-import { createMemoryHistory } from 'history'
-import App from './../../App'
+import Navigation from './Navigation.js'
 
-describe('App.test.js', () => {
-  it('renders Dashboard when the path exactly equals "/"', () => {
-    const { queryByText } = render(
-      <BrowserRouter initialEntries={['/']}>
-        <App />
-      </BrowserRouter>
-    )
-    expect(queryByText('Welcome!')).toBeInTheDocument()
-  })
-
-  it('renders CreatePage when the path exactly equals "/create"', () => {
-    const { queryByText } = render(
-      <BrowserRouter initialEntries={['/recipes']}>
-        <App />
-      </BrowserRouter>
-    )
-    expect(queryByText('Insert a habit')).toBeInTheDocument()
-  })
-
-  it('renders OverviewPage when the path exactly equals "/overview"', () => {
-    const { queryByText } = render(
-      <BrowserRouter initialEntries={['/overview']}>
-        <App />
-      </BrowserRouter>
-    )
-    expect(queryByText('Calendar')).toBeInTheDocument()
-  })
-
-  it('renders 404 Page when the path is non existing', () => {
-    const { queryByText } = render(
-      <BrowserRouter initialEntries={['/fjdlkjfei']}>
-        <App />
-      </BrowserRouter>
-    )
-    expect(queryByText('Oops! Page not found.')).toBeInTheDocument()
-  })
+test('renders correctly', () => {
+  expect(Navigation).toMatchSnapshot()
 })
