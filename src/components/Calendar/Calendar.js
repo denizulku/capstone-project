a460e6e24d3b5fd988c79ffcdd2d025b5f3bd637
denizulku <deniz.uelkue@gmail.com>
2020-09-14 11:06:01 +0200

    Remove redundant const

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,43 +1,14 @@
 import React from 'react'
-import DayPicker, { LocaleUtils } from 'react-day-picker'
+import DayPicker from 'react-day-picker'
 import 'react-day-picker/lib/style.css'
 import styled from 'styled-components/macro'
 
-const MONTHS = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-]
-const WEEKDAYS_LONG = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-]
 const WEEKDAYS_SHORT = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 
 export default function Calendar() {
   return (
     <StyledCalendar>
-      <DayPicker
-        locale="en"
-        months={MONTHS}
-        weekdaysLong={WEEKDAYS_LONG}
-        weekdaysShort={WEEKDAYS_SHORT}
-        firstDayOfWeek={1}
-      />
+      <DayPicker weekdaysShort={WEEKDAYS_SHORT} firstDayOfWeek={1} />
     </StyledCalendar>
   )
 }
