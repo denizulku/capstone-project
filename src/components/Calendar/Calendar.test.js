a6e913451b1b2c01025ba44e15d57230bbcc0e53
denizulku <deniz.uelkue@gmail.com>
2020-09-14 12:02:14 +0200

    Add testing

---
+++ src/components/Calendar/Calendar.test.js
@@ -0,0 +1,13 @@
+import moment from 'moment'
+import renderer from 'react-test-renderer'
+import React from 'react'
+import Calendar from './Calendar'
+
+it('renders without crashing', () => {
+  let props = {
+    currentDay: moment('2017-12-26').format('MMM Do YYYY a'),
+  }
+
+  const tree = renderer.create(<Calendar {...props} />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
