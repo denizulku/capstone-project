3e2bee81c9df901ae038efd78a93561ace78be2f
denizulku <deniz.uelkue@gmail.com>
2020-09-14 10:48:45 +0200

    Change first day of the week

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,7 +1,48 @@
 import React from 'react'
-import DayPicker from 'react-day-picker'
+import DayPicker, { LocaleUtils } from 'react-day-picker'
 import 'react-day-picker/lib/style.css'
+import styled from 'styled-components/macro'
+
+const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+const WEEKDAYS_LONG = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
+const WEEKDAYS_SHORT = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 
 export default function Calendar() {
-  return <DayPicker />
+  return (
+    <StyledCalendar>
+      <DayPicker
+        locale="en"
+        months={MONTHS}
+        weekdaysLong={WEEKDAYS_LONG}
+        weekdaysShort={WEEKDAYS_SHORT}
+        firstDayOfWeek={1}
+      />
+    </StyledCalendar>
+  )
 }
+
+const StyledCalendar = styled.div`
+  display: flex;
+  justify-content: center;
+`
