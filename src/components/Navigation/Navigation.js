4562f74ec71322baf33fba1b5534e8eb8c276fcf
denizulku <deniz.uelkue@gmail.com>
2020-09-23 21:16:12 +0200

    Wip

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,23 +2,24 @@ import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 import { ReactComponent as IconHome } from '../../components/assets/icons/IconHome.svg'
-import { ReactComponent as IconCancel } from '../../components/assets/icons/IconCancel.svg'
+import { ReactComponent as IconAdd } from '../../components/assets/icons/IconAdd.svg'
 import { ReactComponent as IconMonthlyOverview } from '../../components/assets/icons/IconMonthlyOverview.svg'
 
 export default function Navigation() {
-  const [active, setActive] = useState()
   return (
-    <NavigationStyled>
-      <NavLink exact to="/">
-        <IconHome />
-      </NavLink>
-      <NavLink data-cy="create" to="/Create">
-        <AddIcon />
-      </NavLink>
-      <NavLink to="/Overview">
-        <IconMonthlyOverview />
-      </NavLink>
-    </NavigationStyled>
+    <>
+      <NavigationStyled>
+        <NavLink exact to="/">
+          <IconHome />
+        </NavLink>
+        <NavLink data-cy="create" to="/Create">
+          <AddIcon />
+        </NavLink>
+        <NavLink to="/Overview">
+          <IconMonthlyOverview />
+        </NavLink>
+      </NavigationStyled>
+    </>
   )
 }
 
@@ -36,12 +37,9 @@ const NavigationStyled = styled.div`
   background: #bcbbbc;
 `
 
-const AddIcon = styled(IconCancel)`
+const AddIcon = styled(IconAdd)`
   z-index: 100;
   position: relative;
   bottom: 30px;
   filter: drop-shadow(1px 1px 5px rgba(0, 0, 0, 0.3));
-  ::after {
-    transform: rotate(-45deg);
-  }
 `
