22907bf23e2bbbf47048c135ec9bd6080ffa748b
denizulku <deniz.uelkue@gmail.com>
2020-09-27 21:21:01 +0200

    Add toggle animation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import styled from 'styled-components/macro'
 import { ReactComponent as IconHome } from '../../components/assets/icons/IconHome.svg'
 import { ReactComponent as IconCancel } from '../../components/assets/icons/IconCancel.svg'
@@ -7,14 +7,15 @@ import { ReactComponent as IconAdd } from '../../components/assets/icons/IconAdd
 import { ReactComponent as IconMonthlyOverview } from '../../components/assets/icons/IconMonthlyOverview.svg'
 
 export default function Navigation() {
+  const isCreatePath = useLocation().pathname === '/Create' ? 1 : 0
   return (
     <>
       <NavigationStyled>
         <NavLink exact to="/" activeStyle={{ fill: '#ffffff' }}>
           <HomeIconStyled />
         </NavLink>
-        <NavLink data-cy="create" to="/Create">
-          <AddIcon />
+        <NavLink data-cy="create" to={isCreatePath ? '/' : '/Create'}>
+          <AddIcon isCreatePath={isCreatePath} />
         </NavLink>
         <NavLink to="/Overview" activeStyle={{ fill: '#ffffff' }}>
           <OverviewIconStyled />
@@ -51,4 +52,9 @@ const AddIcon = styled(IconAdd)`
   position: relative;
   bottom: 30px;
   filter: drop-shadow(1px 1px 5px rgba(0, 0, 0, 0.3));
+  border-radius: 50%;
+  background-color: ${(props) => (props.isCreatePath ? '#ffb9b9' : '#fff')};
+  transform: ${(props) => (props.isCreatePath ? 'rotate(45deg)' : 'rotate(0)')};
+  transform-origin: 50% 50%;
+  transition: transform 0.3s;
 `
